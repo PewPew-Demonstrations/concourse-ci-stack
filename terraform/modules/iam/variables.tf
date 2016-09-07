acf13c06d94619c37a1e4f7e2f7b20d9d547e59a
Anthony Sceresini <asceresini@gmail.com>
2016-09-07 20:50:40 +1000

    `kms_access_roles` variable should be a list

--- terraform/modules/iam/variables.tf
+++ terraform/modules/iam/variables.tf
@@ -1,7 +1,7 @@
 variable "ecs_worker_role_name" { type = "string" }
 variable "environment" { type = "string" }
 variable "environment_id" { type = "string" }
-variable "kms_access_roles" { type = "string" }
+variable "kms_access_roles" { type = "list" }
 variable "name" { type = "string" }
 variable "owner" { type = "string" }
 variable "role" { type = "string" }
